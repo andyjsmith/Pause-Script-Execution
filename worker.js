4491386cecd5c5672f2921f2ec8242197819554f
Andy Smith <andy@ajsmith.org>
2025-06-26 20:03:50 -0400

    Add console message to show how to fix

--- worker.js
+++ worker.js
@@ -12,7 +12,19 @@ chrome.commands.onCommand.addListener(async (command) => {
 		injectImmediately: true,
 		target: { tabId: currentTab[0].id },
 		func: () => {
+			let before = performance.now();
+
+			// --- Pause script execution ---
 			debugger;
+			// --- Pause script execution ---
+
+			if (performance.now() - before < 50) {
+				console.log(
+					"%c⚠️ How to fix Pause Script Execution ⚠️%c\nIt looks like Pause Script Execution did not work.\n\nTo fix this, open DevTools settings by clicking the ⚙️ settings icon at the top of DevTools (or press F1). Go to 'Ignore list' > 'General exclusion rules', and UNCHECK 'Content scripts injected by extensions'.\n\nThen, reload the page and try again.",
+					"color:red;font-size:large;",
+					""
+				);
+			}
 		},
 	});
 });
